57bef6decff1c9d2d238a91fc73cd3208330f18b
Marin Godechot <marin.godechot@gmail.com>
2017-10-06 12:23:15 +0200

    Exo 1 fini

--- TP3.java
+++ TP3.java
@@ -1,39 +1,39 @@
 import java.util.Scanner;
 
 public class TP3 {
+	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		exo1();
-		scan.close();
 
 	}
 
 	public static void exo1() {
-		Scanner scan = new Scanner(System.in);
 		System.out.println("Tableau uni-dimenssionnel à 4 valeurs :");
+		int[] array = promptIntArray();
+
+		printIntArray(array);
+
+		System.out.println("Tableau à 3 lignes et 4 colonnes :");
+		int[][] bigArray = new int[3][4];
+		for (int i = 0; i < bigArray.length; i++) {
+			System.out.println("Ligne d'indice " + i + " :");
+			bigArray[i] = promptIntArray();
+		}
+		for (int i = 0; i < bigArray.length; i++) {
+			printIntArray(bigArray[i]);
+		}
+
+	}
+
+	public static int[] promptIntArray() {
 		int[] array = new int[4];
 
 		for (int i = 0; i < array.length; i++) {
 			System.out.println("Nombre d'indice " + i + " ?");
 			array[i] = scan.nextInt();
 		}
-
-		printIntArray(array);
-		
-		System.out.println("Tableau à 3 lignes et 4 colonnes :");
-		int[][] bigArray = new int[3][4];
-		for (int i = 0; i < bigArray.length; i++) {
-			for (int t = 0; t < bigArray[i].length; t++){
-				System.out.println("Nombre de la ligne " + i + " et de la colonne " + t + " ?");
-				bigArray[i][t] = scan.nextInt();
-			}
-		}
-		for (int i = 0; i < bigArray.length; i++) {
-			printIntArray(bigArray[i]);
-		}
-		
-		scan.close();
+		return array;
 	}
 
 	public static void printIntArray(int[] array) {
@@ -42,4 +42,5 @@ public class TP3 {
 		}
 		System.out.println();
 	}
+
 }
