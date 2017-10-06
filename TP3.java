d31540e2de0a2032bee9084a6ff096f0659872fe
Marin Godechot <marin.godechot@gmail.com>
2017-10-06 11:12:19 +0200

    Create TP3

---
+++ TP3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TP3 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		int[] array = new int[4];
+		
+		for (int i = 0; i < array.length; i++){
+			System.out.println("Nombre d'indice " + i + " ?");
+			array[i] = scan.nextInt();
+		}
+		
+		printIntArray(array);
+		
+		scan.close();
+
+	}
+	
+	public static void printIntArray(int[] array){
+		for (int i = 0; i < array.length; i++){
+			System.out.print(array[i] + " ");
+		}
+	}
+
+}
