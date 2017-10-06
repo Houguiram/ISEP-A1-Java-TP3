8b073c405a70905e5bfb55bea180488a1b820dbe
Marin Godechot <marin.godechot@gmail.com>
2017-10-06 11:43:16 +0200

    Exo1 part 2

--- TP3.java
+++ TP3.java
@@ -11,6 +11,7 @@ public class TP3 {
 
 	public static void exo1() {
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Tableau uni-dimenssionnel à 4 valeurs :");
 		int[] array = new int[4];
 
 		for (int i = 0; i < array.length; i++) {
@@ -19,6 +20,19 @@ public class TP3 {
 		}
 
 		printIntArray(array);
+		
+		System.out.println("Tableau à 3 lignes et 4 colonnes :");
+		int[][] bigArray = new int[3][4];
+		for (int i = 0; i < bigArray.length; i++) {
+			for (int t = 0; t < bigArray[i].length; t++){
+				System.out.println("Nombre de la ligne " + i + " et de la colonne " + t + " ?");
+				bigArray[i][t] = scan.nextInt();
+			}
+		}
+		for (int i = 0; i < bigArray.length; i++) {
+			printIntArray(bigArray[i]);
+		}
+		
 		scan.close();
 	}
 
@@ -26,5 +40,6 @@ public class TP3 {
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
+		System.out.println();
 	}
 }
